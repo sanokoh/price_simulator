03ceb8c143f69e4f5106dca3f7a3b321355d4e20
sanokoh <nishiki.drop@gmail.com>
2021-05-18 22:39:58 +0900

    first

---
+++ app.vue
@@ -0,0 +1,24 @@
+Vue.createApp({
+	data() {
+		return {
+			monthlyCost: {
+				zius: {
+					template:1100,
+					customize:4400,
+					original:6600,
+					shopping:6600,
+				},
+				server: 1320,
+				ssl: 800, //need to confirm the fee
+			},
+			initCost: {
+				zius: 30000,
+				server: 0,
+				ssl: 880,
+			},
+			yearlyCost: {
+				domain: 1650,
+			},
+		};
+	},
+}).mount('#pricingCalculator');
